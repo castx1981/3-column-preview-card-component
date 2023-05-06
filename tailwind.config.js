299e69136c596dcf3a6b9dde34976bb42e0990e3
Xiaoliang Luan <xluan@qjnext.com>
2023-05-06 16:27:17 +0800

    Restyle based on figma design

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,17 +8,10 @@ module.exports = {
         'cursive': ['"Big Shoulders Display"', 'cursive'],
       },
       colors: {
-        'bright-orange': 'hsl(31, 77%, 52%)',
-        'dark-cyan': 'hsl(184, 100%, 22%)',
-        'very-dark-cyan': 'hsl(179, 100%, 13%)',
-        'transparent-white': 'hsla(0, 0%, 100%, 0.75)',
-        'very-light-gray': 'hsl(0, 0%, 95%)',
-      },
-      fontSize: {
-        'body': ['15px', {
-          lineHeight: '1.625',
-          fontWeight: '500',
-        }],
+        'brightOrange': 'hsl(31, 77%, 52%)',
+        'darkCyan': 'hsl(184, 100%, 22%)',
+        'veryDarkCyan': 'hsl(179, 100%, 13%)',
+        'veryLightGray': 'hsl(0, 0%, 95%)',
       },
     }
   },
